ac8083d6f6299622bc4d50516f82d1fedd36ac8d
Kleyton Lopes <kleyton.slopes@yahoo.com.br>
2018-09-17 22:30:48 -0300

    Inicio Trabalho 3

---
+++ Trabalho3/ConnectionFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Trabalho3
+{
+    public class ConnectionFactory
+    {
+        private readonly String host = "localhost";
+        private readonly String db = "locadora";
+        private readonly String user = "root";
+        private readonly String password = "root";
+
+        private MySqlConnection con;
+
+        public Boolean create()
+        {
+            try
+            {
+                con = new MySqlConnection("Server="+ host + ";Database="+ db + ";Uid="+ user + ";Pwd="+ password + ";SslMode=None;");
+                if (con != null)
+                {
+                    con.Open();
+                    Console.WriteLine("Conectado.");
+                    return true;
+                }
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine("Erro ao conectar a base de dados");
+                throw new InvalidOperationException(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Classe não encontrada, adicione o driver nas bibliotecas.");
+                throw new InvalidOperationException(e.Message);
+            }
+            return false;
+        }
+    }
+}
---
+++ Trabalho3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao sistema de Locadoção”");
+            ConnectionFactory con = new ConnectionFactory();
+            con.create();
+        }
+    }
+}
